2b191337491ad5e1c5535c164599e75fb13ad8cf
AronDerenyi <aron.derenyi@gmail.com>
2019-12-18 18:14:47 +0100

    Added a node builder for kotlin

---
+++ Engine/src/org/xyloto/builder/NodeBuilder.kt
@@ -0,0 +1,21 @@
+package org.xyloto.builder
+
+import org.xyloto.Attribute
+import org.xyloto.Node
+
+inline class NodeBuilder(val node: Node) {
+
+	constructor(vararg attributes: Attribute) : this(Node(*attributes))
+}
+
+inline fun build(vararg attributes: Attribute, init: NodeBuilder.() -> Unit = {}): Node {
+	val builder = NodeBuilder(*attributes)
+	builder.init()
+	return builder.node
+}
+
+inline fun NodeBuilder.node(vararg attributes: Attribute, init: NodeBuilder.() -> Unit = {}): Node {
+	val node = build(*attributes) { init() }
+	this.node.add(node)
+	return node
+}
